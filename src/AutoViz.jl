2d74bc7e708ecf3d0ccfffed5714d89b80f29907
Tim Wheeler <wheelert@stanford.edu>
2016-08-06 11:21:00 -0700

    point to own files

--- src/AutoViz.jl
+++ src/AutoViz.jl
@@ -21,15 +21,15 @@ export
 const DEFAULT_CANVAS_WIDTH = 1000
 const DEFAULT_CANVAS_HEIGHT = 600
 
-include(Pkg.dir("AutomotiveDrivingModels", "src", "viz", "colorscheme.jl"))
-include(Pkg.dir("AutomotiveDrivingModels", "src", "viz", "rendermodels.jl"))
-include(Pkg.dir("AutomotiveDrivingModels", "src", "viz", "camera.jl"))
+include(Pkg.dir("AutoViz", "src", "viz", "colorscheme.jl"))
+include(Pkg.dir("AutoViz", "src", "viz", "rendermodels.jl"))
+include(Pkg.dir("AutoViz", "src", "viz", "camera.jl"))
 
-include(Pkg.dir("AutomotiveDrivingModels", "src", "viz", "render_roadways.jl"))
-include(Pkg.dir("AutomotiveDrivingModels", "src", "viz", "render_vehicles.jl"))
-include(Pkg.dir("AutomotiveDrivingModels", "src", "viz", "render_scenes.jl"))
+include(Pkg.dir("AutoViz", "src", "viz", "render_roadways.jl"))
+include(Pkg.dir("AutoViz", "src", "viz", "render_vehicles.jl"))
+include(Pkg.dir("AutoViz", "src", "viz", "render_scenes.jl"))
 
-include(Pkg.dir("AutomotiveDrivingModels", "src", "viz", "overlays.jl"))
+include(Pkg.dir("AutoViz", "src", "viz", "overlays.jl"))
 
 # include(Pkg.dir("AutomotiveDrivingModels", "src", "viz", "reel_drive.jl"))
 # include(Pkg.dir("AutomotiveDrivingModels", "src", "viz", "gtk.jl"))
