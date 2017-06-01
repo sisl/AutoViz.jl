24b9f216c5850ce6179568d69fbbcd336c26c897
maxiaoba <maxiaoba@umich.edu>
2017-06-01 14:26:55 -0700

    resolve redering for veh with BicycleModel

--- src/2d/vehicles.jl
+++ src/2d/vehicles.jl
@@ -8,3 +8,12 @@ function render!(
     add_instruction!(rendermodel, render_vehicle, (p.x, p.y, p.θ, veh.def.length, veh.def.width, color))
     return rendermodel
 end
+function render!(
+    rendermodel::RenderModel,
+    veh::Entity{VehicleState, BicycleModel, Int},
+    color::Colorant=RGB(rand(), rand(), rand())
+    )
+
+    veh2 = Vehicle(veh.state, veh.def.def, veh.id)
+    render!(rendermodel, veh2, color)
+end
