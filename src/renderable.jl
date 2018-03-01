667255845c83316368e3cdfa7349c6b61f6958c2
Ekhlas Sonu <ekhlas.sonu@gmail.com>
2018-03-01 11:05:20 -0800

    Division of tasks

--- src/renderable.jl
+++ src/renderable.jl
@@ -1,9 +1,10 @@
 #TODO: Renderable type
 abstract type Renderable end
 
-#TODO: default rendering function
-#TODO: rendering text
-#TODO: render()
-#TODO: Tutorial notebook -> roadways, arrow cars, how to convert
-#TODO: test -> run tutorial notebook, default conversions, isrenderable(), etc.
-#TODO: Hints for figuring out rendermodels
+#TODO: default rendering function: Ekhlas: Take abstract vectors of (x,y,θ=0) and make ArrowCar
+#TODO: rendering text: Zach
+#TODO: render(): Zach
+#TODO: Tutorial notebook -> roadways, arrow cars, how to convert: Ekhlas
+#TODO: test -> run tutorial notebook, default conversions, isrenderable(), etc.: Ekhlas
+#TODO: Hints for figuring out rendermodels: Zach
+#TODO: make roadways directly renderable : isrenderable(::Type{Roadway}) = true, render!(): Ekhlas
