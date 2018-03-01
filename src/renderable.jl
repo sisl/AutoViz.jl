20dc921f836626d127fde7f59dd4c7ec867a00b2
Ekhlas Sonu <ekhlas.sonu@gmail.com>
2018-03-01 11:02:46 -0800

    Division of tasks

--- src/renderable.jl
+++ src/renderable.jl
@@ -1,1 +1,9 @@
-#TODO: Renderable type 
+#TODO: Renderable type
+abstract type Renderable end
+
+#TODO: default rendering function
+#TODO: rendering text
+#TODO: render()
+#TODO: Tutorial notebook -> roadways, arrow cars, how to convert
+#TODO: test -> run tutorial notebook, default conversions, isrenderable(), etc.
+#TODO: Hints for figuring out rendermodels
